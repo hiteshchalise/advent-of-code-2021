96019599737526b3425316752ddaf5b1cb5ee03b
Hitesh Chalise <hiteshchalise10@gmail.com>
2021-12-23 11:29:53 +0545

    Day-1 part_1 and part_2 completed.

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,22 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun part1(input: List<Int>): Int {
+        return List(input.size) { index ->
+            input[(index - 1).coerceAtLeast(0)] < input[index]
+        }.count { it }
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part2(input: List<Int>): Int {
+        return List(input.size - 3) { index ->
+            (input[index] + input[index + 1] + input[index + 2]) < (input[index + 1] + input[index + 2] + input[index + 3])
+        }.count { it }
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val testInput = readInput("Day01_test").map(String::toInt)
+    check(part1(testInput) == 7)
+    check(part2(testInput) == 5)
 
-    val input = readInput("Day01")
+    val input = readInput("Day01").map(String::toInt)
     println(part1(input))
     println(part2(input))
 }
